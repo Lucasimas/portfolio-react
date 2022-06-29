96f5fb34b24d76cb48fef69e333827df4872e0eb
Lucas Simas <lucas.bsimas@hotmail.com>
2022-06-29 15:06:26 -0300

    Ajuste submissão form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './contact.css'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsWhatsapp} from 'react-icons/bs'
 import {BsLinkedin} from 'react-icons/bs'
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_laxfl3k', 'template_kbm2z47', form.current, 'YFf69CH78Xn-rZtCL')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
+
+
   return (
     <section id='contact'>
       <h5>Entre em contato</h5>
@@ -37,7 +57,7 @@ const Contact = () => {
         </div>
         {/* Fim opções de contato */}
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Nome' required />
           <input type="email" name='email' placeholder='Email' required />
           <textarea name="message" rows="7" placeholder='Escreva uma mensagem' required ></textarea>
